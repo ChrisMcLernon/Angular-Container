8f7a6b18697efd0f9eadd61ff266b9f95c55c611
Chris McLernon <137217447+ChrisMcLernon@users.noreply.github.com>
2024-06-23 17:58:40 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://03bw8h99w1.execute-api.us-east-1.amazonaws.com’
+'
 };
